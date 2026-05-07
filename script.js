3e08bd3028c4af76a90dd2579ba63fa8b0649b42
NirmalkumarS2002 <nirmalsiva555@gmail.com>
2026-05-07 20:28:01 +0530

    errors resolved

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 // menu toggle
 
 // let doggle=document.querySelector(".menu-toggle");
-    
+
 //     let visible=false;
 
 // doggle.addEventListener("click",()=>{
@@ -52,44 +52,66 @@ reveals.forEach(el => {
 // form page
 
 
-const submit = document.querySelector(".form-submit-button");
+const submit =document.querySelector(".form-submit-button");
 
 
 submit.addEventListener("click", (e) => {
 
     e.preventDefault();
 
-    const nameInput = document.querySelector("#name").value.trim();
-    const companyInput = document.querySelector("#company").value.trim();
-    const emailInput = document.querySelector("#email").value.trim();
-    const subjectInput = document.querySelector("#subject").value;
-    const messageInput = document.querySelector("#message").value.trim();
+    const nameInput =document.querySelector("#name").value.trim();
+    const companyInput =document.querySelector("#company").value.trim();
+     const emailInput =document.querySelector("#email").value.trim();
+    const subjectInput =document.querySelector("#subject").value;
+    const messageInput =document.querySelector("#message").value.trim();
+
+    const nameRegex = /^[A-Za-z\s]+$/;
+    const emailRegex =/^[^\s@]+@[^\s@]+\.[^\s@]+$/;
 
     if (nameInput === "") {
-        alert("Please Enter Your Name")
+        alert("Please Enter Your Name");
+        return;
+    }
+
+    if (!nameRegex.test(nameInput)) {
+        alert(
+            "Name should contain only alphabets"
+        );
         return;
     }
 
     if (companyInput === "") {
-        alert("Please Enter Your Name")
+        alert("Please Enter Company Name");
+        return;
+    }
+
+    if (!nameRegex.test(companyInput)) {
+        alert(
+            "Company name should contain only alphabets");
         return;
     }
 
     if (emailInput === "") {
-        alert("Please Enter Your Email")
+        alert("Please Enter Your Email");
         return;
     }
+
+    if (!emailRegex.test(emailInput)) {
+        alert("Please Enter Valid Email");
+        return;
+    }
+
 
     if (subjectInput === "") {
-        alert("Please Select Subject")
+        alert("Please Select Subject");
         return;
     }
+
 
     if (messageInput === "") {
-        alert("Please Enter Your Message")
+        alert("Please Enter Your Message");
         return;
     }
-
 
     alert("Submitted Successfully!");
 
@@ -98,22 +120,22 @@ submit.addEventListener("click", (e) => {
     document.querySelector("#email").value = "";
     document.querySelector("#subject").selectedIndex = 0;
     document.querySelector("#message").value = "";
-
 });
+
 
 /* LOADER HIDE AFTER 3 SECONDS */
 
-window.addEventListener("load",()=>{
+window.addEventListener("load", () => {
 
     let loader =
-    document.querySelector(".loader-container");
+        document.querySelector(".loader-container");
 
-    setTimeout(()=>{
+    setTimeout(() => {
 
         loader.style.opacity = "0";
 
         loader.style.visibility = "hidden";
 
-    },2000);
+    }, 2000);
 
 });
